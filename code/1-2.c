eb9a81505f7f02bbdb0e55e9ee10e4ead3675219
danhokang <danho2898@naver.com>
2026-04-10 09:49:23 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int arr2[2][4] = {1,2,3,4,5,6,7,8};
+    int arr2[2][4] = {{1,2,3,4}, {5,6,7,8}};
     int (*p)[4];
 
     for (int i=0; i<2; i++) {
-        p = arr2 + i;
+        p = arr2 + i;   // assign만 arr2 사용 가능
+
         for (int j=0; j<4; j++) {
-            printf("%d ", *(*(p) + j));
+            printf("%d\n", *(*(p) + j));
         }
-        printf("\n");
     }
 }
